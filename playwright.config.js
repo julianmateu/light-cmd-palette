47416dfe89fa763659e2c908cf37a5a6491e280a
Julian Mateu <julianmateu@gmail.com>
2024-01-31 13:26:36 +0000

    Parameterize tests, add more browsers

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,51 @@
+import { defineConfig, devices } from '@playwright/test'
+
 const PORT = 8080
 
-export const webServer = {
-  command: `npx http-server test/fixtures -p ${PORT}`,
-  port: PORT
-}
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './',
+  /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
+  snapshotDir: './__snapshots__',
+  /* Maximum time one test can run for. */
+  timeout: 10 * 1000,
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+    baseUrl: `http://localhost:${PORT}`
+  },
 
-export const use = {
-  baseUrl: `http://localhost:${PORT}`
-}
+  webServer: {
+    command: `npx http-server test/fixtures -p ${PORT}`,
+    port: PORT,
+    reuseExistingServer: !process.env.CI
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    },
+    {
+      name: 'edge',
+      use: { ...devices['Desktop Edge'] }
+    }
+  ]
+})
--- test/commandPalette.spec.js
+++ test/commandPalette.spec.js
@@ -1,61 +1,71 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Command Palette should', () => {
-  async function openCommandPalette (page) {
-    await page.keyboard.down('Control')
-    await page.keyboard.down('Shift')
-    await page.keyboard.press('H')
-    await page.keyboard.up('Shift')
-    await page.keyboard.up('Control')
-  }
+const UrlToKeyboardShortcuts = {
+  '/': 'H',
+  '/custom-shortcut.html': 'P'
+}
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/')
-    await openCommandPalette(page)
-  })
+// run a parameterized test
 
-  test('be visible', async ({ page }) => {
-    const commandPalette = await page.$('#command-palette')
-    expect(commandPalette).toBeTruthy()
-    const isVisible = await commandPalette.isVisible()
-    expect(isVisible).toBeTruthy()
-  })
+for (const [url, shortcut] of Object.entries(UrlToKeyboardShortcuts)) {
+  test.describe(`Command Palette with shortcut ${shortcut} should`, () => {
+    async function openCommandPalette (page) {
+      await page.keyboard.down('Control')
+      await page.keyboard.down('Shift')
+      await page.keyboard.press(shortcut)
+      await page.keyboard.up('Shift')
+      await page.keyboard.up('Control')
+    }
 
-  test('display an alert when selecting "Alert" command with Enter', async ({ page }) => {
-    // Assuming "Alert" is the second command in the list
-    await page.keyboard.press('ArrowDown') // Navigate to "Alert"
-    await page.keyboard.press('Enter') // Select "Alert"
+    test.beforeEach(async ({ page }) => {
+      await page.goto(url)
+      await openCommandPalette(page)
+    })
 
-    page.once('dialog', async dialog => {
-      expect(dialog.message()).toBe('Executed Command 2')
-      await dialog.dismiss()
+    test('be visible', async ({ page }) => {
+      const commandPalette = await page.$('#command-palette')
+      expect(commandPalette).toBeTruthy()
+      const isVisible = await commandPalette.isVisible()
+      expect(isVisible).toBeTruthy()
+    })
+
+    test('display an alert when selecting "Alert" command with Enter', async ({ page }) => {
+      // Assuming "Alert" is the second command in the list
+      await page.keyboard.press('ArrowDown')
+      await page.keyboard.press('ArrowDown') // Navigate to "Alert"
+      await page.keyboard.press('Enter') // Select "Alert"
+
+      page.once('dialog', async dialog => {
+        expect(dialog.message()).toBe('Executed Command 2')
+        await dialog.dismiss()
+      })
+    })
+
+    test('display an alert when selecting "Alert" command with mouse click', async ({ page }) => {
+      // Assuming "Alert" is the second command in the list
+      const alertCommand = await page.$('#command-list li:nth-child(2)')
+      await alertCommand.click()
+
+      page.once('dialog', async dialog => {
+        expect(dialog.message()).toBe('Executed Command 2')
+        await dialog.dismiss()
+      })
+    })
+
+    test('display all commands', async ({ page }) => {
+      const commandList = await page.$$('#command-list li')
+      expect(commandList.length).toBe(2)
+      expect(await commandList[0].innerText()).toBe('Print')
+      expect(await commandList[1].innerText()).toBe('Alert')
+    })
+
+    test('display only matching commands when filtering', async ({ page }) => {
+      const commandInput = await page.$('#command-input')
+      await commandInput.fill('A')
+
+      const commandList = await page.$$('#command-list li')
+      expect(commandList.length).toBe(1)
+      expect(await commandList[0].innerText()).toBe('Alert')
     })
   })
-
-  test('display an alert when selecting "Alert" command with mouse click', async ({ page }) => {
-    // Assuming "Alert" is the second command in the list
-    const alertCommand = await page.$('#command-list li:nth-child(2)')
-    await alertCommand.click()
-
-    page.once('dialog', async dialog => {
-      expect(dialog.message()).toBe('Executed Command 2')
-      await dialog.dismiss()
-    })
-  })
-
-  test('display all commands', async ({ page }) => {
-    const commandList = await page.$$('#command-list li')
-    expect(commandList.length).toBe(2)
-    expect(await commandList[0].innerText()).toBe('Print')
-    expect(await commandList[1].innerText()).toBe('Alert')
-  })
-
-  test('display only matching commands when filtering', async ({ page }) => {
-    const commandInput = await page.$('#command-input')
-    await commandInput.fill('A')
-
-    const commandList = await page.$$('#command-list li')
-    expect(commandList.length).toBe(1)
-    expect(await commandList[0].innerText()).toBe('Alert')
-  })
-})
+}
